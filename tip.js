3f429e96d6e451f7b48e7adfe12efdfeaef5850d
Azem <38106778+Azemchap@users.noreply.github.com>
2022-09-09 05:06:02 +0100

    first commit

---
+++ tip.js
@@ -0,0 +1,217 @@
+
+// input
+const billInput = document.getElementById('bill-input')
+const peopleInput = document.getElementById('people-input')
+const tipInput = document.getElementById('tip-custom')
+const tipBtns = document.querySelectorAll('.tip')
+const pepleError = document.querySelector('.person-wrapper h4')
+const billError = document.querySelector('.bill-wrapper h4')
+
+//Reset
+const resetBtn = document.querySelector('.reset-btn')
+
+// Results
+const tip = document.querySelector('.tip-per-person')
+const totalTip = document.querySelector('.tip-total')
+const billPerPerson = document.querySelector('.bill-per-person')
+const totalBill = document.querySelector('.bill-total')
+
+
+// Test
+// pepleError.classList.add('block')
+// billInput.addEventListener('change', () => {
+//     let bill = billInput.value
+//     console.log(bill)
+// })
+// peopleInput.addEventListener('change', () => {
+//     console.log(peopleInput.value)
+// })
+// tipInput.addEventListener('change', () => {
+//     console.log(tipInput.value)
+// })
+
+
+// Basic Structure of how the Codes will run 
+billInput.addEventListener('change', setBillValue)
+peopleInput.addEventListener('change', setPeopleValue)
+tipInput.addEventListener('change', setCustomValue)
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', handleTip)
+})
+
+resetBtn.addEventListener('click', reset)
+
+// Give Default Values for your Variables (useful when you reset)
+let billValue = 0.00
+let tipValue = 0.05 //default value 5% btn is active
+let peopleValue = 1
+
+// Function to validate Floats and Intergers 
+function validateFloat(s) {
+    var rgx = /^[0-9]*\.?[0-9]*$/
+    return s.match(rgx)
+}
+
+function validateInt(s) {
+    var rgx = /^[0-9]*$/
+    return s.match(rgx)
+}
+
+
+
+
+
+// Function to get Bill Amoount
+function setBillValue() {
+
+
+    billValue = Number(billInput.value)
+    // console.log(billValue)
+    // console.log(typeof billValue)
+
+    if (billValue <= 0) {
+        billError.classList.add('error')
+        // setTimeout(function () {
+        //     billError.classList.remove('error')
+        // }, 3000)
+    } else if (billValue > 0) {
+        billError.classList.remove('error')
+        calculateTip()
+    }
+
+}
+
+
+
+function handleTip(e) {
+    tipBtns.forEach(btn => {
+        //clear active state
+        tipInput.classList.remove('active')
+        btn.classList.remove('active')
+
+        //set active state
+        if (e.target.innerText == btn.innerText) {
+            btn.classList.add('active')
+            // Number Method to parse just the number
+            tipValue = parseFloat(btn.innerText) / 100
+            // console.log(tipValue)
+            // console.log(typeof tipValue)
+        }
+    })
+
+
+
+    //clear custom tip
+    tipInput.value = ''
+
+    calculateTip()
+
+}
+
+
+
+
+
+function setCustomValue() {
+
+    tipValue = parseFloat(tipInput.value / 100)
+
+    // remove active state from buttons
+    tipBtns.forEach(btn => {
+        btn.classList.remove('active')
+    })
+
+    tipInput.classList.add('active')
+    // console.log(tipValue)
+    // console.log(typeof tipValue)
+
+    if (tipInput.value !== '') {
+        calculateTip()
+    }
+    else if (billInput.value !== '') {
+        calculateTip()
+    }
+
+}
+
+
+function setPeopleValue() {
+    if (!validateInt(peopleInput.value)) {
+        peopleInput.value = peopleInput.value.substring(0, peopleInput.value.length - 1)
+
+
+    }
+
+    peopleValue = Number(peopleInput.value)
+    // console.log(peopleValue)
+    // console.log(typeof peopleValue)
+
+    if (peopleValue <= 0) {
+        pepleError.classList.add('error')
+        // setTimeout(function () {
+        //     pepleError.classList.remove('error')
+        // }, 3000)
+
+    } else {
+        pepleError.classList.remove('error')
+        calculateTip()
+    }
+
+}
+
+
+function calculateTip() {
+    // if peopleValue and billValue are greater than 1 and 0 respectively
+    if (peopleValue >= 1 && billValue >= 0) {
+
+        let tipAmount = billValue * tipValue / peopleValue
+        let totalTipAmount = billValue * tipValue
+
+        // tip per person
+        tip.innerText = tipAmount.toFixed(2)
+
+        // total tip
+        totalTip.innerText = totalTipAmount.toFixed(2)
+
+        // bill per person 
+        billPerPerson.innerText = ((billValue / peopleValue) + tipAmount).toFixed(2)
+
+        // total bill 
+        totalBill.innerText = (billValue + totalTipAmount).toFixed(2)
+    }
+}
+
+
+function reset() {
+    // resetBillValue()
+    billInput.value = ''
+    // resetTipValue()
+    tipInput.value = ''
+    // resetPeopleValue()
+    peopleInput.value = ''
+
+
+    // clear active state from tipInput and all % btns
+    tipInput.classList.remove('active')
+    tipBtns.forEach(btn => {
+        btn.classList.remove('active')
+    })
+    // add active state to default 5% btn
+    tipBtns[0].classList.add('active')
+
+
+    // reset all results outputs to zero (0.00) 
+    tip.innerText = '0.00'
+    totalTip.innerText = '0.00'
+    billPerPerson.innerText = '0.00'
+    totalBill.innerText = '0.00'
+}
+
+
+
+
+
+
+
+
+
